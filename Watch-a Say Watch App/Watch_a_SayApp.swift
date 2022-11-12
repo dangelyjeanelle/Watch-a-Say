9c3303f0a17d99ef907fd34dede9227c5cbe0ccf
Dangely Canabal <djeanelle0110@gmail.com>
2022-11-12 18:24:05 -0500

    Initial Commit

---
+++ Watch-a Say Watch App/Watch_a_SayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_a_SayApp.swift
+//  Watch-a Say Watch App
+//
+//  Created by Dangely Canabal on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Watch_a_Say_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Watch-a Say/Watch_a_SayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_a_SayApp.swift
+//  Watch-a Say
+//
+//  Created by Dangely Canabal on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Watch_a_SayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
